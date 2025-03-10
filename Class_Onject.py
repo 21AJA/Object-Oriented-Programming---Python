e33f5e0561b1b6bcfc678251f96c0bf91ffdfefd
Ajay Kumar Maurya <ajaykumarmaurya760@gmail.com>
2025-03-10 15:50:22 +0530

    initial commit

---
+++ Class_Onject.py
@@ -0,0 +1,18 @@
+# Python Class and object with Constructor
+
+class student: #Class
+    def __init__(self,name, branch):#Constructor
+        self.name = name
+        self.branch = branch 
+    
+stu1=student("Akshay","IT") #Object
+print(stu1.name)
+print(stu1.branch)
+
+#python class and object without showing constructor
+
+class cricketer:
+    openor = "shikhar dhawan"
+    bowller = "Jaspreet bumrah"
+name = cricketer()
+print(name.openor)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+class student: #Class
+    def __init__(self,name, branch):#Constructor
+        self.name = name
+        self.branch = branch 
+    
+stu1=student("Akshay","IT") #Object
+print(stu1.name)
+print(stu1.branch)
