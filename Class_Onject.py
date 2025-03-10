2bf35f0a60c414eb6c1018f03f4b7f382c496979
Ajay Kumar Maurya <ajaykumarmaurya760@gmail.com>
2025-03-10 20:49:18 +0530

    initial commit

--- Class_Onject.py
+++ Class_Onject.py
@@ -9,10 +9,23 @@ stu1=student("Akshay","IT") #Object
 print(stu1.name)
 print(stu1.branch)
 
-#python class and object without showing constructor
+# #python class and object without showing constructor
 
 class cricketer:
     openor = "shikhar dhawan"
     bowller = "Jaspreet bumrah"
 name = cricketer()
 print(name.openor)
+print(name.bowller)
+
+#Class & Object with methods
+class student:
+    def __init__(self,name,branch):
+        self.name=name
+        self.branch=branch
+    def hello(self):
+        print("this is method in python")
+stu1 = student("karan","IT")
+stu1.hello()
+print(stu1.name)
+print(stu1.branch)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,17 @@
 class student: #Class
-    def __init__(self,name, branch):#Constructor
-        self.name = name
-        self.branch = branch 
+#     def __init__(self,name, branch):#Constructor
+#         self.name = name
+#         self.branch = branch 
     
-stu1=student("Akshay","IT") #Object
-print(stu1.name)
-print(stu1.branch)
+# stu1=student("Akshay","IT") #Object
+# print(stu1.name)
+# print(stu1.branch)
+
+# #python class and object without showing constructor
+
+# class cricketer:
+#     openor = "shikhar dhawan"
+#     bowller = "Jaspreet bumrah"
+# name = cricketer()
+# print(name.openor)
+# print(name.bowller)
